b87dcc0c91baa90eb96d63745b9d2063f07750a8
CodyJReed <cody2j2reed@gmail.com>
2019-09-11 12:37:34 -0400

    sequence, iterative, conditional

--- helloWorld.go
+++ helloWorld.go
@@ -4,4 +4,16 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello")
+	foo()
+	fmt.Println("something more.")
+
+	for i := 0; i < 10; i++ {
+		if i%2 == 0 {
+			fmt.Println("This is an even number", i)
+		}
+	}
+}
+
+func foo() {
+	fmt.Println("I'm in foo")
 }
